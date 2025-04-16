bc32d82dba2588a58ae5b80a8be3246f4db058ab
Kavin <kavin1615yoga@gmail.com>
2025-04-16 18:35:11 +0530

    added some features to the form

--- src/main/java/com/springmvc/thymeleaf/controller/StudentController.java
+++ src/main/java/com/springmvc/thymeleaf/controller/StudentController.java
@@ -1,14 +1,27 @@
 package com.springmvc.thymeleaf.controller;
 
 import com.springmvc.thymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
+
 
     @GetMapping("/showStudentFrom")
     public String showForm(Model model){
@@ -18,6 +31,12 @@ public class StudentController {
 
         //add student object as a model attribute
         model.addAttribute("student",theStudent);
+
+        model.addAttribute("countries",countries);
+
+        model.addAttribute("languages",languages);
+
+        model.addAttribute("systems",systems);
 
         return "student-form";
     }
--- src/main/java/com/springmvc/thymeleaf/model/Student.java
+++ src/main/java/com/springmvc/thymeleaf/model/Student.java
@@ -1,10 +1,18 @@
 package com.springmvc.thymeleaf.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
 
     private String lastName;
+
+    private String country;
+
+    private String programmingLanguage;
+
+    private List<String> operatingSystem;
 
     public Student(){}
 
@@ -22,5 +30,29 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public List<String> getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(List<String> operatingSystem) {
+        this.operatingSystem = operatingSystem;
     }
 }
